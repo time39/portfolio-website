3475924a442e7c290a85a51096b540175b74f0f4
Tim Bracht <tim.bracht13100@gmail.com>
2022-09-01 23:05:27 +0200

    inital

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import {Route, Routes} from 'react-router-dom'
+import Home from './components/Home'
+import About from "./components/About";
+import {createMuiTheme} from "@material-ui/core";
 
 function App() {
+    let theme = React.useMemo(
+        () =>
+            createMuiTheme({
+                palette: {
+                    primary: {
+                        light: '#00e5ff',
+                        main: '#2916F5',
+                        dark: '#00004f',
+
+                    },
+                },
+                typography: {
+                    h1: {
+                        fontFamily: '"Coolvetica',
+                        textTransform: 'uppercase',
+                        // fontSize:'calc(2rem + 4vw)'
+                    },
+                    button: {
+                        textTransform: ''
+                    }
+                }
+            }),
+        [],
+    );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+    </Routes>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import './index.css'
+import {NavLink} from "react-router-dom";
+import React from "react";
+
+const Navbar = () => (
+    <nav className={'nav'}>
+        <NavLink className={'title'} to={'/'}>
+            Tim Bracht
+        </NavLink>
+        <ul>
+            <li>
+                <NavLink className={'links'} to={'/about'}>About</NavLink>
+            </li>
+            <li>
+                <NavLink className={'links'} to={'/contact'}>Contact</NavLink>
+            </li>
+        </ul>
+    </nav>
+)
+export default Navbar
