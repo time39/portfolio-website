cfe47583adf9c252b3ca082864173d1441761129
timbracht <tim.bracht13100@gmail.com>
2022-09-15 20:22:27 +0200

    about page styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Navbar from "../Navbar";
+import './index.css'
 
 const About = () => {
-    return(
+    return (
         <div>
             <Navbar/>
-            Hello About
+            <div className={'container'}>
+                <div className={'study box'}>
+                    <img src={"school.png"}/>
+                    <h2>
+                        Here is a text for my studies. Here is a text for my studies.
+                        Here is a text for my studies. Here is a text for my studies. Here is a text for my studies.
+                    </h2>
+                </div>
+                <div className={'experience box'}>
+                    <img src={"buildings.png"}/>
+                    <h2>
+                        Here is a text for my studies. Here is a text for my studies.
+                        Here is a text for my studies. Here is a text for my studies. Here is a text for my studies.
+                    </h2>
+                </div>
+                <div className={'skills box'}>
+                    <img src={"programming.png"}/>
+                    <h2>
+                        Here is a text for my studies. Here is a text for my studies.
+                        Here is a text for my studies. Here is a text for my studies. Here is a text for my studies.
+                    </h2>
+                </div>
+            </div>
+
         </div>
     )
 }
